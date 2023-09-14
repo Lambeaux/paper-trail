0f5eece961437752fb0b3fb1203395b2d259b746
Steven Lombardi <lambeauxworks@gmail.com>
2023-09-13 21:54:02 -0700

    Migrate trace-seq to use context maps

--- src/lambeaux/paper_trail/seq.clj
+++ src/lambeaux/paper_trail/seq.clj
@@ -5,39 +5,40 @@
 (def temp-ctx {::pt/current-ns *ns*})
 
 (defn pre-process
-  [forms prev dlogs scope]
-  (loop [[exp & exprs] forms
-         prev* prev
-         dlogs* dlogs]
+  [ctx]
+  (loop [{:keys [forms] [exp & exprs] :input :as ctx*} ctx]
     (cond
-      (nil? exp) [prev* (list) dlogs* scope]
-      :else (recur exprs (conj prev* exp) dlogs*))))
+      (nil? exp) ctx*
+      :else (recur (assoc ctx*
+                          :input exprs
+                          :forms (conj forms exp))))))
 
 (defn trace-seq
   ([form scope]
-   (apply trace-seq
-          (pre-process
-           (tree-seq seqable? seq form) (list) [] scope)))
-  ([forms args dlogs scope]
-   (loop [[exp & exprs] forms
-          args* args
-          dlogs* dlogs]
+   (trace-seq
+    (pre-process {:input (tree-seq seqable? seq form)
+                  :forms (list)
+                  :args (list)
+                  :scope scope})))
+  ([ctx]
+   (loop [{:keys [args scope] [exp & exprs] :forms :as ctx*} ctx]
      (cond
        (nil? exp)
        nil
        (and (symbol? exp)
             (contains? scope exp))
-       (recur exprs (conj args* (scope exp)) dlogs*)
+       (recur (assoc ctx*
+                     :forms exprs
+                     :args (conj args (scope exp))))
        (list? exp)
        (let [exp-size (count exp)
              result (apply (core/->impl temp-ctx (first exp))
-                           (map (core/map-impl-fn temp-ctx) 
-                                (rest (take exp-size args*))))]
-         (cons {:form (take exp-size args*) :result result}
+                           (map (core/map-impl-fn temp-ctx)
+                                (rest (take exp-size args))))]
+         (cons {:form (take exp-size args) :result result}
                (lazy-seq
-                (trace-seq (cons result exprs) 
-                           (drop exp-size args*) 
-                           dlogs* 
-                           scope))))
+                (trace-seq (assoc ctx*
+                                  :forms (cons result exprs)
+                                  :args (drop exp-size args))))))
        :else
-       (recur exprs (conj args* exp) dlogs*)))))
+       (recur (assoc ctx* :forms exprs :args (conj args exp)))))))
