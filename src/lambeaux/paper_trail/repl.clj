0b0ecabc068252dfaf7b9291a32812dc97bb153a
Steven Lombardi <lambeauxworks@gmail.com>
2025-01-19 19:09:47 -0700

    Add some standard repl tooling

--- src/lambeaux/paper_trail/repl.clj
+++ src/lambeaux/paper_trail/repl.clj
@@ -1,4 +1,31 @@
-(ns lambeaux.paper-trail.repl)
+(ns lambeaux.paper-trail.repl
+  (:require [lambeaux.paper-trail.decomp :as ptd]))
+
+(def default-requires
+  ['[lambeaux.paper-trail.repl :as r]
+   '[lambeaux.paper-trail.classpath :as cp]
+   '[lambeaux.paper-trail.decomp :as impl]])
+
+(defn require-pt
+  []
+  (apply require default-requires))
+
+(defmacro with-std-out
+  [& body]
+  `(binding [*out* (java.io.PrintWriter. System/out)]
+     ~@body))
+
+(comment
+
+  (def my-try-form
+    '(let [x (atom 1)]
+       (try
+         (swap! x inc)
+         x
+         (catch Exception e nil)
+         (finally (swap! x inc)))))
+
+  (ptd/run-eval my-try-form))
 
 (comment
   (require '[portal.api :as p])
