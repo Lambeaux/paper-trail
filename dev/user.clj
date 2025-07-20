b38a5e5c512892b7684f5ab65e04d13ed481336f
Steven Lombardi <lambeauxworks@gmail.com>
2025-07-20 13:51:03 -0700

    Checking to ensure the old versions still work

--- dev/user.clj
+++ dev/user.clj
@@ -12,3 +12,15 @@
       result)))
 
 (comment (refresh))
+
+(comment "How to run the old interpreter versions for comparison purposes."
+
+         (require '[lambeaux.paper-trail.misc.impl-v1-rdi :as ptv1]
+                  '[lambeaux.paper-trail.misc.impl-v2-btsi :as ptv2]
+                  '[lambeaux.paper-trail.misc.samples :as samples])
+
+         (ptv1/trace* 'samples/simplest-even
+                      [1 2 3 4 5 6 7])
+
+         (ptv2/trace-seq '(mapv inc (filter odd? (range 10)))
+                         {}))
