fd61becf303ca7fd01714c4f063fcf756a101fdf
Steven Lombardi <lambeauxworks@gmail.com>
2025-07-20 15:54:19 -0700

    Setup license

--- dev/lambeaux/paper_trail/wip/interop_proto.clj
+++ dev/lambeaux/paper_trail/wip/interop_proto.clj
@@ -1,3 +1,10 @@
+;; Copyright (c) Steven Lombardi and Contributors. All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (https://choosealicense.com/licenses/epl-1.0/)
+;; which can be found in the file LICENSE at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
 (ns lambeaux.paper-trail.wip.interop-proto)
 
 (defprotocol InterpretedFn
